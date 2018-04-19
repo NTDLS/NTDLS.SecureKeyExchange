1247f3f189e4d7a4ce10a402685e2b2f25609fbd
Josh Patterson <11428567+NTDLS@users.noreply.github.com>
2018-04-19 14:32:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SecureKeyExchange
+{
+    class Program
+    {
+        static void Test()
+        {
+            SecureKeyNegotiator localHost = new SecureKeyNegotiator();
+            SecureKeyNegotiator foreignHost = new SecureKeyNegotiator();
+
+            byte[] negotiationToken = localHost.GenerateNegotiationToken();
+
+            byte[] negotiationReply = foreignHost.ApplyNegotiationToken(negotiationToken);
+
+            localHost.ApplyNegotiationResponseToken(negotiationReply);
+
+            if (foreignHost.SharedSecret != localHost.SharedSecret)
+            {
+                throw new Exception("This should never happen.");
+            }
+
+            Console.WriteLine("  Local Public:{0}", localHost.PublicNumber);
+            Console.WriteLine("Foreign Public:{0}", foreignHost.PublicNumber);
+            Console.WriteLine("L Share Secret:{0}", localHost.SharedSecret);
+            Console.WriteLine("F Share Secret:{0}", foreignHost.SharedSecret);
+        }
+
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 100000; i++)
+            //{
+                Test();
+            //    Console.Write(i.ToString("N0") + "\r");
+            //}
+
+            //Console.WriteLine("Done!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SecureKeyExchange")]
+[assembly: AssemblyDescription("Secure Key Exchange implementation of Diffie-Hellman.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NetworkDLS")]
+[assembly: AssemblyProduct("SecureKeyExchange")]
+[assembly: AssemblyCopyright("Copyright © 2018 NetworkDLS")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("41d8a197-e709-4953-b17e-6b24be5a4f3a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
