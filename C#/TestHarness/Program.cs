bf86e5caa739e97f7f910bb406959b6c329b5ca2
Josh Patterson <josh@ntdls.com>
2022-11-18 15:06:10 -0500

    Cleanup.

--- C#/TestHarness/Program.cs
+++ C#/TestHarness/Program.cs
@@ -10,39 +10,51 @@ namespace TestHarness
             var localHost = new UnitNegotiator();
             byte[] negotiationToken = localHost.GenerateNegotiationToken();
 
-            var foreignHost = new UnitNegotiator();
-            byte[] negotiationReply = foreignHost.ApplyNegotiationToken(negotiationToken);
+            var remotePeer = new UnitNegotiator();
+            byte[] negotiationReply = remotePeer.ApplyNegotiationToken(negotiationToken);
 
             localHost.ApplyNegotiationResponseToken(negotiationReply);
 
-            if (foreignHost.SharedSecretHash != localHost.SharedSecretHash)
+            if (remotePeer.SharedSecretHash != localHost.SharedSecretHash)
             {
                 throw new Exception("This should never happen.");
             }
 
             Console.WriteLine($"Key length: {localHost.KeyLength} bytes.");
-            Console.WriteLine($"  Local Shared Secret: {localHost.SharedSecretHash}");
-            Console.WriteLine($"Foreign Shared Secret: {foreignHost.SharedSecretHash}");
+            Console.WriteLine($" Local Shared Secret: {localHost.SharedSecretHash}");
+            Console.WriteLine($"Remote Shared Secret: {remotePeer.SharedSecretHash}");
         }
 
         static void TestCompoundNegotiator()
         {
+            //Scenerio (use your imagination):
+            //  localHost is a local service.
+            //  remotePeer is a remote peer.
+
+            //localHost starts the process with a call to GenerateNegotiationToken(),
+            //  specifying the size of the key (which is actually n*12).
             var localHost = new CompoundNegotiator();
             byte[] negotiationToken = localHost.GenerateNegotiationToken(8);
 
-            var foreignHost = new CompoundNegotiator();
-            byte[] negotiationReply = foreignHost.ApplyNegotiationToken(negotiationToken);
+            //localHost passes the resulting bytes from GenerateNegotiationToken()
+            //  to a remote peer which passes the bytes to ApplyNegotiationToken().
+            var remotePeer = new CompoundNegotiator();
+            byte[] negotiationReply = remotePeer.ApplyNegotiationToken(negotiationToken);
 
+            //The remotePeer passes the bytes from ApplyNegotiationToken() back to the 
+            //  localHost, where the localHost passes them to ApplyNegotiationResponseToken()
             localHost.ApplyNegotiationResponseToken(negotiationReply);
 
-            if (foreignHost.SharedSecretHash != localHost.SharedSecretHash)
+            //At this point, both the localHost and the remotePeer have the same bytes in
+            //  "SharedSecret" as we can see from comparing the "SharedSecretHash".
+            if (remotePeer.SharedSecretHash != localHost.SharedSecretHash)
             {
                 throw new Exception("This should never happen.");
             }
 
             Console.WriteLine($"Key length: {localHost.KeyLength} bytes.");
-            Console.WriteLine($"  Local Shared Secret: {localHost.SharedSecretHash}");
-            Console.WriteLine($"Foreign Shared Secret: {foreignHost.SharedSecretHash}");
+            Console.WriteLine($" Local Shared Secret: {localHost.SharedSecretHash}");
+            Console.WriteLine($"Remote Shared Secret: {remotePeer.SharedSecretHash}");
         }
 
         static void Main(string[] args)
