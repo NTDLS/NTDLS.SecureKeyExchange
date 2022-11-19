b7e2d7e5b1e01ef07b60d86d885e087573886f1b
Josh Patterson <josh@ntdls.com>
2022-11-19 12:34:37 -0500

    nuget package release.

--- C#/NTDLS.SecureKeyExchange/UnitNegotiator.cs
+++ C#/NTDLS.SecureKeyExchange/UnitNegotiator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 
 namespace NTDLS.SecureKeyExchange
